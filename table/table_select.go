99664d1225f013cb933f696a060453eef38f5c51
go-rowan <arvinmulifern@gmail.com>
2025-12-29 00:24:30 +0700

    feat(table): add drop method

--- table/table_select.go
+++ table/table_select.go
@@ -30,3 +30,40 @@ func (t *Table) Select(cols ...string) (*Table, error) {
 		length:  t.length,
 	}, nil
 }
+
+func (t *Table) Drop(cols ...string) (*Table, error) {
+	argsCount := len(cols)
+	if argsCount == 0 {
+		return t.copy(), nil
+	}
+
+	dropSet := make(map[string]struct{}, argsCount)
+	for _, c := range cols {
+		if _, ok := t.data[c]; !ok {
+			return nil, fmt.Errorf("drop: column %s does not exist", c)
+		}
+
+		dropSet[c] = struct{}{}
+	}
+
+	data := make(map[string][]any)
+	columns := make([]string, 0, len(t.columns))
+
+	for _, c := range t.columns {
+		if _, drop := dropSet[c]; drop {
+			continue
+		}
+
+		values := make([]any, len(t.data[c]))
+		copy(values, t.data[c])
+
+		data[c] = values
+		columns = append(columns, c)
+	}
+
+	return &Table{
+		columns: columns,
+		data:    data,
+		length:  t.length,
+	}, nil
+}
