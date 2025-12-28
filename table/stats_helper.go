b98608db798ee6b14187233df8f8cc5c57be72c3
go-rowan <arvinmulifern@gmail.com>
2025-12-28 23:42:08 +0700

    feat(table): add column stats methods

---
+++ table/stats_helper.go
@@ -0,0 +1,14 @@
+package table
+
+func numeric(v any) (float64, bool) {
+	switch n := v.(type) {
+	case int:
+		return float64(n), true
+	case int64:
+		return float64(n), true
+	case float64:
+		return n, true
+	default:
+		return 0, false
+	}
+}
