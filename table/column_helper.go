ce6483e2da9e1b0787b6597e380ebdfb6df2ba22
go-rowan <arvinmulifern@gmail.com>
2025-12-29 17:41:58 +0700

    feat(table): add categorize method

---
+++ table/column_helper.go
@@ -0,0 +1,27 @@
+package table
+
+func inferCategorical(data []any, maxUnique int) bool {
+	if len(data) == 0 {
+		return false
+	}
+
+	switch data[0].(type) {
+	case bool:
+		return false
+	}
+
+	uniques := make(map[any]struct{})
+	for _, d := range data {
+		uniques[d] = struct{}{}
+		if len(uniques) > maxUnique {
+			return false
+		}
+	}
+
+	switch data[0].(type) {
+	case string, int, int64:
+		return true
+	default:
+		return false
+	}
+}
--- table/table_ops.go
+++ table/table_ops.go
@@ -28,3 +28,46 @@ func (t *Table) MapCol(name string, f func(any) any) (*Table, error) {
 		length:  t.length,
 	}, nil
 }
+
+func (t *Table) Categorize() *Table {
+	columnsCount := len(t.columns)
+	data := make(map[string][]any, columnsCount*2)
+	columns := make([]string, 0, columnsCount*2)
+
+	for _, c := range t.columns {
+		originData := t.data[c]
+
+		d := make([]any, len(originData))
+		copy(d, originData)
+		data[c] = d
+
+		columns = append(columns, c)
+
+		col, _ := t.Col(c)
+		if !col.categorical {
+			continue
+		}
+
+		ctgMap := make(map[any]int)
+		ctgData := make([]any, len(originData))
+		index := 0
+
+		for i, v := range originData {
+			if _, ok := ctgMap[v]; !ok {
+				ctgMap[v] = index
+				index++
+			}
+			ctgData[i] = ctgMap[v]
+		}
+
+		headerName := c + "_categorized"
+		data[headerName] = ctgData
+		columns = append(columns, headerName)
+	}
+
+	return &Table{
+		columns: columns,
+		data:    data,
+		length:  t.length,
+	}
+}
