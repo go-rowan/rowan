eea62ad1125bdaeaa95341e43002366c73b38065
go-rowan <arvinmulifern@gmail.com>
2026-05-05 07:47:49 +0700

    feat(table): add rename columns

--- table/table_mutate.go
+++ table/table_mutate.go
@@ -20,6 +20,12 @@ func (t *Table) RenameColumn(oldName, newName string) error {
 		return fmt.Errorf("rename: column %s already exists", newName)
 	}
 
+	renameColumn(t, oldName, newName, values)
+
+	return nil
+}
+
+func renameColumn(t *Table, oldName, newName string, values []any) {
 	delete(t.data, oldName)
 	t.data[newName] = values
 
@@ -28,6 +34,46 @@ func (t *Table) RenameColumn(oldName, newName string) error {
 			t.columns[i] = newName
 			break
 		}
+	}
+}
+
+// RenameColumns renames multiple columns based on the provided map (oldName -> newName).
+//
+// If any rename is invalid, no changes are applied to the table.
+func (t *Table) RenameColumns(nameMap map[string]string) error {
+	usedColumns := make(map[string]struct{})
+	originalValues := make(map[string][]any)
+
+	for _, c := range t.columns {
+		if _, isAssigned := nameMap[c]; !isAssigned {
+			usedColumns[c] = struct{}{}
+		}
+	}
+
+	for oldName, newName := range nameMap {
+		if oldName == newName {
+			continue
+		}
+
+		values, ok := t.data[oldName]
+		if !ok {
+			return fmt.Errorf("rename: column %s does not exist", oldName)
+		}
+		originalValues[oldName] = values
+
+		if _, exists := usedColumns[newName]; exists {
+			return fmt.Errorf("rename: column name collision for %s", newName)
+		}
+
+		usedColumns[newName] = struct{}{}
+	}
+
+	for oldName, newName := range nameMap {
+		if oldName == newName {
+			continue
+		}
+
+		renameColumn(t, oldName, newName, originalValues[oldName])
 	}
 
 	return nil
